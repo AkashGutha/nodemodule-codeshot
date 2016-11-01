d455891d283ec6e82f41cae7fa40a8c80eccaa5d
AkashGutha <akash.gutha@outlook.com>
2016-11-01 10:19:36 +0530

    removed flavored-path and added some skeloetal code

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // include required node modules.
 var fs = require('fs');
-var path = require('flavored-path');
 var childProcess = require('child_process');
 
-module.exports = function () {
-    
+var codeShot = function () {
+
 }
+
+codeShot.prototype.capture = function (path, quality = 10, width = 600, height = 400) {
+
+}
+
+module.exports = codeShot;
